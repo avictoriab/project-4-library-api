6b8039383bd6c75f74744872ae820cb28c83745f
avictoriab <victoriabellomo7@hotmail.com>
2026-08-06 20:21:29 -0300

    feat: configure SQLAlchemy database setup

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = "books"
+    
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+
