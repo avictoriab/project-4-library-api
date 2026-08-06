017273e4df0d029186517bde38eea89fcb85d1d1
avictoriab <victoriabellomo7@hotmail.com>
2026-08-06 19:42:42 -0300

    feat: implement basic GET and POST book endpoints

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+books = []
 
 @app.route("/")
 def home():
@@ -8,6 +10,23 @@ def home():
         "name": "Library API",
         "version": "1.0",
         "status": "running"
+    })
+
+@app.route("/books")
+def get_books():
+    return jsonify({
+        "books": books
+    })
+
+@app.route("/books", methods=["POST"])
+def create_book():
+    data = request.get_json()
+
+    books.append(data)
+
+    return jsonify({
+        "message": "Book created",
+        "book": data
     })
 
 if __name__ == "__main__":
