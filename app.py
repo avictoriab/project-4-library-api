7da3b1fedfdf8200ea0f01ad5d597301c74729c2
avictoriab <victoriabellomo7@hotmail.com>
2026-08-08 11:42:38 -0300

    feat: connect book creation endpoint to database

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request
+from database import Session
+from models import Book
 
 app = Flask(__name__)
-
-books = []
 
 @app.route("/")
 def home():
@@ -14,20 +14,44 @@ def home():
 
 @app.route("/books")
 def get_books():
+    session = Session()
+
+    books = session.query(Book).all()
+
+    session.close()
+
     return jsonify({
-        "books": books
+        "books": [
+            {
+            "id": book.id,
+            "title": book.title,
+            "author": book.author
+            }
+            for book in books
+        ]
     })
 
 @app.route("/books", methods=["POST"])
 def create_book():
+    session = Session()
     data = request.get_json()
+    
+    book = Book(
+        title = data['title'],
+        author = data['author']
+    )
 
-    books.append(data)
+    session.add(book)
+    session.commit()
 
-    return jsonify({
-        "message": "Book created",
-        "book": data
-    })
+    response = {
+        "id": book.id,
+        "title": book.title,
+        "author": book.author
+    }
+    session.close()
+
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(debug=True)
